1ef12d2c721359237c81e02e87ac7468c9c677da
Anny He <anny.he@salesforce.com>
2019-06-14 12:09:50 -0700

    update to use Lightning Data Service

--- force-app/main/default/lwc/youTubePlayerRecordWrapper/youTubePlayerRecordWrapper.js
+++ force-app/main/default/lwc/youTubePlayerRecordWrapper/youTubePlayerRecordWrapper.js
@@ -1,20 +1,19 @@
-import { LightningElement, api, track, wire } from 'lwc';
-import getFieldValueFromRecordIdFieldName from '@salesforce/apex/YouTubeController.getFieldValueFromRecordIdFieldName';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 
 export default class YouTubePlayerRecordWrapper extends LightningElement {
     @api fieldName;
+    @api objectApiName;
     @api recordId;
-    @track youTubeId;
-    @track error;
 
-    @wire(getFieldValueFromRecordIdFieldName, { recordId: '$recordId', fieldName: '$fieldName' })
-    wiredFieldValue({ error, data }) {
-        if (data) {
-            this.youTubeId = data;
-            this.error = '';
-        } else if (error) {
-            this.error = error;
-            this.youTubeId = '';
-        } 
+    @wire(getRecord, { recordId: '$recordId', fields: '$fieldNames' })
+    record;
+
+    get youTubeId() {
+        return this.record.data ? this.record.data.fields[this.fieldName].value : '';
     }
-}
+
+    get fieldNames() {
+        return [this.objectApiName + '.' + this.fieldName];
+    }
+} 
