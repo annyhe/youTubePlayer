7f90449a1fabe01c5ce80c2ef13e1f9e9f17a165
Anny He <anny.he@salesforce.com>
2019-06-19 11:47:57 -0700

    update all non-variable usages of YouTube to reference YouTube, case-sensitive

--- force-app/main/default/lwc/basicYouTubePlayer/basicYouTubePlayer.js
+++ force-app/main/default/lwc/basicYouTubePlayer/basicYouTubePlayer.js
@@ -1,28 +1,28 @@
 import { LightningElement, api } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import youTubePath from '@salesforce/resourceUrl/YouTubeJS';
+import YouTubePath from '@salesforce/resourceUrl/YouTubeJS';
 
 export default class BasicYouTubePlayer extends LightningElement {
     @api youTubeId;
     player;
 
-    youTubePathInitialized = false; 
+    YouTubePathInitialized = false;
     renderedCallback() {
-        if (this.youTubePathInitialized || !this.youTubeId) {
+        if (this.YouTubePathInitialized || !this.youTubeId) {
             return;
         }
 
         Promise.all([
-            loadScript(this, youTubePath + '/iframe_api.js'),
-            loadScript(this, youTubePath + '/widget_api.js'),
-        ]) 
-        .then(() => {
-            this.onYouTubeIframeAPIReady();
-        })
-        .catch((error) => {
-            this.showErrorToast(error);
-        });
+            loadScript(this, YouTubePath + '/iframe_api.js'),
+            loadScript(this, YouTubePath + '/widget_api.js')
+        ])
+            .then(() => {
+                this.onYouTubeIframeAPIReady();
+            })
+            .catch(error => {
+                this.showErrorToast(error);
+            });
     }
 
     onPlayerError(e) {
@@ -30,21 +30,24 @@ export default class BasicYouTubePlayer extends LightningElement {
         if (e.data === 2) {
             explanation = 'Invalid YouTube ID';
         } else if (e.data === 5) {
-            explanation = 'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.';
+            explanation =
+                'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.';
         } else if (e.data === 100) {
-            explanation = 'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.';
+            explanation =
+                'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.';
         } else if (e.data === 101 || e.data === 150) {
-            explanation = 'The owner of the requested video does not allow it to be played in embedded players.';
+            explanation =
+                'The owner of the requested video does not allow it to be played in embedded players.';
         }
-        
+
         this.showErrorToast(explanation);
-    }    
+    }
 
     showErrorToast(explanation) {
         const evt = new ShowToastEvent({
             title: 'Error loading YouTube player',
             message: explanation,
-            variant: 'error',
+            variant: 'error'
         });
         this.dispatchEvent(evt);
     }
@@ -54,7 +57,7 @@ export default class BasicYouTubePlayer extends LightningElement {
         let playerElem = this.template.querySelector('.player');
         if (!playerElem) {
             const containerElem = this.template.querySelector('.wrapper');
-            playerElem = document.createElement("DIV");   
+            playerElem = document.createElement('DIV');
             playerElem.className = 'player';
             containerElem.appendChild(playerElem);
         }
@@ -62,10 +65,10 @@ export default class BasicYouTubePlayer extends LightningElement {
         this.player = new YT.Player(playerElem, {
             height: '390',
             width: '100%',
-            videoId: this.youTubeId,            
+            videoId: this.youTubeId,
             events: {
-              'onError': this.onPlayerError.bind(this)
+                onError: this.onPlayerError.bind(this)
             }
-        });        
+        });
     }
 }
