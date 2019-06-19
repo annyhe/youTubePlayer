cba69aae03a303687bea6dedff3589b126418422
Anny He <anny.he@salesforce.com>
2019-06-18 22:34:10 -0700

    add error message

--- force-app/main/default/lwc/basicYouTubePlayer/basicYouTubePlayer.js
+++ force-app/main/default/lwc/basicYouTubePlayer/basicYouTubePlayer.js
@@ -1,9 +1,11 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import youTubePath from '@salesforce/resourceUrl/Archive';
 
 export default class BasicYouTubePlayer extends LightningElement {
     @api youTubeId;
+    @track error;
+    player;
 
     youTubePathInitialized = false;
     get youTubeUrl () {
@@ -15,26 +17,11 @@ export default class BasicYouTubePlayer extends LightningElement {
         }
 
         window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this);
-        window.onPlayerReady = function(e) {
+        window.onPlayerReady = function() {
             console.log('on Player ready');
-            e.target.playVideo();
         }
 
-        window.onErrorFunc = function(e) {
-            let explanation = '';
-            if (e.data === 2) {
-                explanation = 'invalid YouTube ID';
-            } else if (e.data === 5) {
-                explanation = 'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.';
-            } else if (e.data === 100) {
-                explanation = 'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.';
-            } else if (e.data === 101 || e.data === 150) {
-                explanation = 'The owner of the requested video does not allow it to be played in embedded players.';
-            }
-            
-            console.log('explanation is: ' + explanation);
-            //   document.getElementById('explanation').innerHTML = explaination;
-        }    
+        window.onPlayerError = this.onPlayerError.bind(this);
 
         Promise.all([
             loadScript(this, youTubePath + '/iframe_api.js'),
@@ -48,18 +35,42 @@ export default class BasicYouTubePlayer extends LightningElement {
         });
     }
 
+    onPlayerError(e) {
+        let explanation = '';
+        if (e.data === 2) {
+            explanation = 'invalid YouTube ID';
+        } else if (e.data === 5) {
+            explanation = 'The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.';
+        } else if (e.data === 100) {
+            explanation = 'The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.';
+        } else if (e.data === 101 || e.data === 150) {
+            explanation = 'The owner of the requested video does not allow it to be played in embedded players.';
+        }
+        
+        console.log('explanation is: ' + explanation);
+        this.error = explanation;
+    }    
+
     onYouTubeIframeAPIReady() {
-        let player = new YT.Player(this.template.querySelector('.player'), {
+        // if this is called repeatedly it will add divs repeatedly. check if the playerElem exists before creating new elements
+        let playerElem = this.template.querySelector('.player');
+        if (!playerElem) {
+            const containerElem = this.template.querySelector('.wrapper');
+            playerElem = document.createElement("DIV");   
+            playerElem.className = 'player';
+            containerElem.appendChild(playerElem);
+        }
+
+        this.player = new YT.Player(playerElem, {
             height: '390',
             width: '100%',
             videoId: this.youTubeId,            
             events: {
               'onReady': window.onPlayerReady,
-              'onStateChange': window.onPlayerStateChange
+              'onError': window.onPlayerError
             }
         });        
 
         console.log('in onYouTubeIframeAPIReady:');
-        console.log(player);
     }
 }
