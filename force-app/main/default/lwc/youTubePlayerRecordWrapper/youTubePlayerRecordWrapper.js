e728181cce9280974071cda19439f88c675edce1
Anny He <anny.he@salesforce.com>
2019-06-19 11:37:24 -0700

    prettify LWC components

--- force-app/main/default/lwc/youTubePlayerRecordWrapper/youTubePlayerRecordWrapper.js
+++ force-app/main/default/lwc/youTubePlayerRecordWrapper/youTubePlayerRecordWrapper.js
@@ -10,7 +10,9 @@ export default class YouTubePlayerRecordWrapper extends LightningElement {
     record;
 
     get youTubeId() {
-        return this.record.data ? this.record.data.fields[this.fieldName].value : '';
+        return this.record.data
+            ? this.record.data.fields[this.fieldName].value
+            : '';
     }
 
     get fieldNames() {
